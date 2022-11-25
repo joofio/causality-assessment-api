dcbc52914d03833dd361d316930e762d9e1eb520
Joao Almeida <joaofilipe90@gmail.com>
2022-11-25 11:35:24 +0000

    rgbl added

--- app/packages.R
+++ app/packages.R
@@ -1,5 +1,5 @@
-#if (!requireNamespace("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager")
-#BiocManager::install(c("graph", "RBGL", "Rgraphviz"))
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c("graph", "RBGL", "Rgraphviz"))
 
 install.packages(c("plumber", "dplyr","logger","jsonlite","yaml","fs","gRain"))
